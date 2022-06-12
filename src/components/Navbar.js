289a4deba0bad70fb9b72add42dfb7fffb03ffa0
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-11 23:06:41 -0400

    set up css variables, routing, and initial navbar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction='row'
+      justifyContent='space-around'
+      sx={{
+        gap: { sm: '123px', xs: '40px' },
+        mt: { sm: '32px', xs: '20px' },
+        justifyContent: 'none',
+      }}
+      px='20px'
+    >
+      <Link to='/'>
+        <img
+          src={Logo}
+          alt='logo for Woodard Training'
+          style={{ width: '150px', height: 'auto', margin: '0px 20px' }}
+        />
+      </Link>
+      <Stack
+        direction='row'
+        gap='40px'
+        fontFamily='Alegreya'
+        fontSize='24px'
+        alignItems='flex-end'
+      >
+        <Link
+          to='/'
+          style={{
+            textDecoration: 'none',
+            color: '#102a42',
+            borderBottom: '3px solid #bb2525',
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href='#exercises'
+          style={{ textDecoration: 'none', color: '#102a42' }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Navbar;
