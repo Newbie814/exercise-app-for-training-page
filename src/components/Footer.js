c2d67e774e4c09fa85c3cb1e9c8f3e2729370a1f
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-13 19:18:49 -0400

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <Box mt='80px' bgcolor='#063251'>
+    <Stack
+      gap='40px'
+      width='100vw'
+      sx={{ alignItems: 'center' }}
+      flexWrap='wrap'
+      px='40px'
+      pt='24px'
+    >
+      <img src={Logo} alt='logo' style={{ width: '200px', height: '41px' }} />
+    </Stack>
+    <Typography
+      variant='h5'
+      sx={{ fontSize: { lg: '16px', xs: '12px' } }}
+      mt='41px'
+      textAlign='center'
+      pb='40px'
+    >
+      All rights reserved
+    </Typography>
+  </Box>
+);
 
 export default Footer;
